764756f043c1cbf25ffc6928f768f51c9957f2f9
ROHANrajendra <rohan30raj@yahoo.co.in>
2020-08-24 18:48:19 +0530

    change_dp trial

--- Week 5 - Dynamic programming/change_dp.py
+++ Week 5 - Dynamic programming/change_dp.py
@@ -1,10 +1,27 @@
 # Uses python3
 import sys
+import math
 
 def get_change(m):
     #write your code here
-    return m // 4
+    denom = [1, 3, 4]
+    ways = [0] * (m +1)
+    ways[0] = 0
+    
+    for i in range(1, m+1):
+        # setting high value as default for comparision
+        ways[i] = math.inf
+        for coin in denom:
+            if i >= coin: 
+                temp = ways[i - coin] + 1
+                if temp < ways[i]:
+                    ways[i] = temp
+            else:
+                break
+                
+    return ways[m]
 
 if __name__ == '__main__':
+    
     m = int(sys.stdin.read())
     print(get_change(m))
