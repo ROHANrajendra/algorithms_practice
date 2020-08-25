03e81fda5842c31c547130d1ccf7cab46b3ed8d4
ROHANrajendra <rohan30raj@yahoo.co.in>
2020-08-25 12:15:07 +0530

    knapsack problem w numpy

--- Week 6 - Dynamic Programming/golden_knapsack.py
+++ Week 6 - Dynamic Programming/golden_knapsack.py
@@ -1,12 +1,22 @@
 # Uses python3
 import sys
+import numpy
 
 def optimal_weight(W, w):
     # write your code here
+    # weights = [[0 for x in range(len(w))] for x in range(W+1)]
+    weights = matrix = numpy.zeros(shape=(len(w), W + 1), dtype=int)
     result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
+    for i in range(len(w)):
+        for j in range(1, W+1):
+            if w[i] > j:
+                weights[i, j] = weights[i-1, j]
+            else:
+                maxval = max(w[i] + weights[i-1, j - w[i]], 
+                    weights[i - 1, j]
+                )
+                weights[i, j] = maxval
+    result = weights[len(w) - 1, W]
     return result
 
 if __name__ == '__main__':
